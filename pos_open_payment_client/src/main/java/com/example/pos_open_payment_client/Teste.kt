3eaa38e2a70ba23a2bf6ab03fdda26e30ac6d465
DENIS\denis <denishdg@gmail.com>
2023-05-22 16:01:31 -0300

    teste 1 commit

---
+++ pos_open_payment_client/src/main/java/com/example/pos_open_payment_client/Teste.kt
@@ -0,0 +1,10 @@
+package com.example.pos_open_payment_client
+
+import android.content.Context
+import android.widget.Toast
+
+class Teste {
+    fun banana (string : String, context: Context){
+        Toast.makeText(context, "testes banana", Toast.LENGTH_LONG).show()
+    }
+}
